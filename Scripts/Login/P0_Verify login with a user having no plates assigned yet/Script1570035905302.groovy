c2d45146d1159516eeff41b1a42a80f94508ee6b
Ayush <ayush.kumar@reviver.com>
2019-10-03 16:25:07 +0530

    1 new TC committed.

---
+++ Scripts/Login/P0_Verify login with a user having no plates assigned yet/Script1570035905302.groovy
@@ -0,0 +1,53 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('/Users/ayush/Desktop/APKs/Rplate26sept120.apk', true)
+
+CustomKeywords.'com.reviver.utils.CommonUtils.selectEnv_Mobile'(GlobalVariable.selectENV, 5)
+
+Mobile.waitForElementPresent(findTestObject('StartPage/android.widget.TextView0 - SIGN IN'), 5)
+
+Mobile.tap(findTestObject('StartPage/android.widget.TextView0 - SIGN IN'), 0)
+
+Mobile.setText(findTestObject('LoginPage/android.widget.EditText0 - Enter email'), findTestData(GlobalVariable.DataFile).getValue(
+        1, 2), 0)
+
+Mobile.setText(findTestObject('LoginPage/android.widget.EditText0 - Enter password'), findTestData(GlobalVariable.DataFile).getValue(
+        2, 2), 0)
+
+Mobile.tap(findTestObject('LoginPage/android.widget.Button0 - SIGN IN'), 0)
+
+Mobile.waitForElementPresent(findTestObject('Homepage/Burger Icon'), 10)
+
+Mobile.verifyElementVisible(findTestObject('Homepage/No plate image'), 0)
+
+Mobile.verifyElementAttributeValue(findTestObject('Homepage/no plate text 3'), 'text', 'Note: This may take 1–3 business days from when your order is processed.', 
+    0)
+
+Mobile.verifyElementAttributeValue(findTestObject('Homepage/no plate text1'), 'text', 'It looks like your plates haven’t been added to your account.', 
+    0)
+
+Mobile.verifyElementAttributeValue(findTestObject('Homepage/no plate text2'), 'text', 'You will be notified when your plate has been added to your account.', 
+    0)
+
+Mobile.verifyElementVisible(findTestObject('Homepage/No Plates Found text'), 0)
+
+Mobile.tap(findTestObject('Homepage/Burger Icon'), 0)
+
+Mobile.verifyElementAttributeValue(findTestObject('Burger Menu/Notifications'), 'checkable', 'true', 0)
+
+
+Mobile.closeApplication()
+
---
+++ Test Suites/Sanity Android APP.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sanity Android APP</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>ayush.kumar@reviver.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>0</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fc8f035b-ab62-4f32-b01d-83607c1c1a9d</testSuiteGuid>
+   <testCaseLink>
+      <guid>d407fd09-9983-4623-a8ba-194c53cc940d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/P0_Verify User can enter valid password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>84a11acb-74a1-4691-9d74-88cfbc91062b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/P0_Verify User can enter invalid password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8678e4da-8a28-42b7-88d5-9b0917524b13</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/P0_Verify login with a user having no plates assigned yet</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
